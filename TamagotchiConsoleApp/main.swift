e464f2c2cd66605cf388b62746af3e1b583f9ce2
Sritej Tummuru <tummuru.s@etoncollege.org.uk>
2022-12-02 13:08:06 +0000

    Used Tamagotchi class in a game format with user interaction.

--- TamagotchiConsoleApp/main.swift
+++ TamagotchiConsoleApp/main.swift
@@ -7,4 +7,29 @@
 
 import Foundation
 
+let tamagotchi = Tamagotchi()
 
+var running = true
+
+let options = ["Eat a snack", "Go to the toilet", "Quit Game"]
+
+while running {
+    print("What would you like your tamagotchi to do?")
+    for (num, optionText) in options.enumerated() {
+        print("\(num + 1). \(optionText)")
+        if let userInput = readLine() {
+            if let userOption = Int(userInput) {
+                switch userOption {
+                    case 1:
+                        tamagotchi.eatSnack(snackSize: 5)
+                    case 2:
+                        tamagotchi.poo()
+                    case 3:
+                        running = false
+                    default:
+                        print("Choose another option.")
+                }
+            }
+        }
+    }
+}
